fc28d4fa1eeede7236f8b71812465064b4f5945c
Amine <Amine>
2016-05-10 00:17:34 +0100

    sprint1

---
+++ app/src/main/java/com/iit/glidapp/Activity2.java
@@ -0,0 +1,59 @@
+package com.iit.glidapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Activity2 extends AppCompatActivity {
+
+    private TextView mInfoText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Log.v("iit", "Activity2 onCreate called");
+        setContentView(R.layout.activity_2_layout);
+        mInfoText = (TextView) findViewById(R.id.info_text);
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            //String textFromMain = getIntent().getStringExtra(MainActivity.INFO_KEY);
+            String textFromMain = bundle.getString(MainActivity.INFO_KEY);
+            if (textFromMain != null) {
+                mInfoText.setText(textFromMain);
+            }
+        }
+    }
+
+    protected void onStart() {
+        super.onStart();
+        Log.v("iit", "Activity2 onStart called");
+    }
+
+
+    protected void onResume() {
+        super.onResume();
+        Log.v("iit", "Activity2 onResume called");
+    }
+
+    protected void onPause() {
+        super.onPause();
+        Log.v("iit", "Activity2 onPause called");
+    }
+
+    protected void onStop() {
+        super.onStop();
+        Log.v("iit", "Activity2 onStop called");
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.v("iit", "Activity2 onDestroy called");
+    }
+
+
+}
---
+++ app/src/main/java/com/iit/glidapp/MainActivity.java
@@ -0,0 +1,131 @@
+package com.iit.glidapp;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public static final String INFO_KEY = "info";
+
+    private RelativeLayout mMainLayout;
+    private Button mButton;
+    private EditText mTopText;
+    private TextView mBelowText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Log.v("iit", "onCreate called");
+        setContentView(R.layout.activity_main);
+
+        mMainLayout = (RelativeLayout) findViewById(R.id.main_layout);
+        mButton = (Button) findViewById(R.id.button);
+        mButton.setOnClickListener(this);
+
+        mTopText = (EditText) findViewById(R.id.top_text);
+        mBelowText = (TextView) findViewById(R.id.below_text);
+        mBelowText.setOnClickListener(this);
+
+    }
+
+    protected void onStart() {
+        super.onStart();
+        Log.v("iit", "onStart called");
+    }
+
+
+    protected void onResume() {
+        super.onResume();
+        Log.v("iit", "onResume called");
+    }
+
+    protected void onPause() {
+        super.onPause();
+        Log.v("iit", "onPause called");
+    }
+
+    protected void onStop() {
+        super.onStop();
+        Log.v("iit", "onStop called");
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.v("iit", "onDestroy called");
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button:
+                buttonClicked();
+                break;
+            case R.id.below_text:
+                belowTextClicked3();
+                break;
+        }
+    }
+
+    private void buttonClicked() {
+        Toast.makeText(this, "buttonclicked", Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(getApplicationContext(), Activity2.class);
+        //intent.putExtra(INFO_KEY,mTopText.getText().toString());
+        Bundle bundle = new Bundle();
+        bundle.putString(INFO_KEY, mTopText.getText().toString());
+        intent.putExtras(bundle);
+        startActivity(intent);
+    }
+
+    private void belowTextClicked() {
+        Log.v("IIT", "text clicked");
+        TextView textView = new TextView(getApplicationContext());
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        layoutParams.addRule(RelativeLayout.CENTER_IN_PARENT);
+        textView.setLayoutParams(layoutParams);
+        textView.setTextColor(Color.YELLOW);
+        textView.setText("salut");
+
+
+        mMainLayout.addView(textView);
+    }
+
+    private void belowTextClicked2() {
+        if (mTopText.getVisibility() == View.GONE) {
+            mTopText.setVisibility(View.VISIBLE);
+        } else {
+            mTopText.setVisibility(View.GONE);
+        }
+    }
+
+    private void belowTextClicked3() {
+        //Uri webpage = Uri.parse("http://www.iit-nau.com/");
+        //Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
+        //startActivity(webIntent);
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, "This is my text to send.");
+        sendIntent.setType("text/plain");
+        startActivity(sendIntent);
+
+    }
+
+    private void launchActivity3() {
+
+        Intent launchActivity3Intent = new Intent("android.iit.test.activity3");
+
+        startActivity(launchActivity3Intent);
+
+    }
+
+}
